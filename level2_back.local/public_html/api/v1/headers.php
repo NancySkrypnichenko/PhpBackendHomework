539cd90204ee6396f93165f3564f46db8c138f66
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-01 12:02:30 +0300

    all headers in one place

---
+++ level2_back.local/public_html/api/v1/headers.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: http://level2.local");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
