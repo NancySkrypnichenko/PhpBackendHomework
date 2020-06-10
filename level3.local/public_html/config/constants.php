fb9502899abc7721aefc380e42408b8bfeb2cdc3
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-10 15:37:13 +0300

    migration works, but should try do it with PDO

---
+++ level3.local/public_html/config/constants.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'backend_user');
+define('DB_PASSWORD', '1605');
+define('DB_NAME', 'library_base');
+define('DB_TABLE_VERSIONS', 'versions');
+
+define ('ROOT', dirname(__FILE__));
+
+define('SRC_PATH', __DIR__ . '/');
+define('OFFSET', 20);
---
+++ level3.local/public_html/sql/0000_base.sql
@@ -0,0 +1,45 @@
+-- Create all start tables --
+
+-- Table versions --
+create table if not exists `versions`
+(
+    `id`      serial       not null,
+    `name`    varchar(255) not null,
+    `created` timestamp default current_timestamp,
+    primary key (id)
+)
+    engine = innodb
+    character set utf8
+    collate utf8_general_ci;
+
+-- Table books --
+create table if not exists `books`
+(
+    `id`              serial       not null,
+    `book_name`       varchar(255) not null,
+    `year`            int(4),
+    `picture`         varchar(255),
+    `author_name`     varchar(255) not null,
+    `number_of_clicks` INT(11) UNSIGNED DEFAULT 0
+)
+
+
+#     `author_id`   BIGINT UNSIGNED NOT NULL,
+#         FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
+#         ON DELETE RESTRICT ON UPDATE CASCADE
+
+    engine = innodb
+    character set utf8
+    collate utf8_general_ci;
+
+
+# -- Table authors --
+# create table if not exists `authors`
+# (
+#     `id`          serial       not null,
+#     `author_name` varchar(255) not null,
+#     `author_last_name`        varchar(255) not null
+# )
+#     engine = innodb
+#     character set utf8
+#     collate utf8_general_ci;
