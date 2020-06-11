e7a92a02d2592e3c3724ab020133f55211f91a2f
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-11 23:37:58 +0300

    connect db was fixed

--- level3.local/public_html/config/connect_bd.php
+++ level3.local/public_html/config/connect_bd.php
@@ -6,6 +6,7 @@ function connectDB() {
     try{
         $db_connect = new PDO('mysql:host=localhost;dbname=library_base;charset=utf8', DB_USER, DB_PASSWORD);
         $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //$db_connect ->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     } catch (PDOException $e) {
         echo json_encode(array("error" => $e->getMessage()));
     }
