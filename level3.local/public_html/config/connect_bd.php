f935c4d02050c8d86f3c768331b0b7049a200c86
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-16 15:46:13 +0300

    it used PDO now

--- level3.local/public_html/config/connect_bd.php
+++ level3.local/public_html/config/connect_bd.php
@@ -1,14 +1,26 @@
 <?php
 require_once 'constants.php';
 
-function connectDB() {
-    $db_connect= null;
-    try{
-        $db_connect = new PDO('mysql:host=localhost;dbname=library_base;charset=utf8', DB_USER, DB_PASSWORD);
-        $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //$db_connect ->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-    } catch (PDOException $e) {
-        echo json_encode(array("error" => $e->getMessage()));
+class ConnectBD
+{
+    private static $instances = null;
+
+    private function __construct()
+    {
     }
-    return $db_connect;
+
+
+    public static function connectDB()
+    {
+        if (self::$instances === null) {
+            try {
+                self::$instances = new PDO('mysql:host=localhost;dbname=library_base;charset=utf8', DB_USER, DB_PASSWORD);
+                self::$instances->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //$db_connect ->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+            } catch (PDOException $e) {
+                echo json_encode(array("error" => $e->getMessage()));
+            }
+        }
+        return self::$instances;
+    }
 }
