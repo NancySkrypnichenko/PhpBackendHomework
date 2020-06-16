6fba07776cfb7149a6b278f7a388151adda3919a
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-16 15:46:59 +0300

    'ROOT' was changed

--- level3.local/public_html/config/constants.php
+++ level3.local/public_html/config/constants.php
@@ -4,8 +4,8 @@ define('DB_USER', 'backend_user');
 define('DB_PASSWORD', '1605');
 define('DB_NAME', 'library_base');
 define('DB_TABLE_VERSIONS', 'versions');
-
-define ('ROOT', dirname(__FILE__));
+define('DB_TABLE_BOOKS', 'books');
+define('ROOT', '/home/nancy/www/level3.local/public_html/');
 
 define('SRC_PATH', __DIR__ . '/');
-define('OFFSET', 20);
+define('OFFSET', 10);
