05305d5e60b7363bfa292d731a5d090952f0e120
NancySkrypnichenko <drachnancy@gmail.com>
2020-06-29 18:05:24 +0300

    upload file was added

---
+++ level3.local/public_html/static/JS/loadFile.js
@@ -0,0 +1,36 @@
+function addFile(event) {
+    var message = $('#load');
+    var formData = new FormData();
+    formData.append('file', $('#picture')[0].files[0]);
+
+
+    $.ajax({
+        type: "POST",
+        url: "/adm/saveFile",
+        data: formData,
+        dataType: "json",
+        cache: false,
+        contentType: false,
+        processData: false,
+        beforeSend: function () {
+            message.text("Запрос начат");
+            $('#picture').prop("disabled", true);
+            $('#submit').prop("disabled", true);
+        },
+        success: function (data) {
+            if (data.status == 'ok') {
+                message.html("Файл загружен<br>" +
+                    "<img src='../../static/PreFiles/" + data.fileName + "'>" +
+                    "<input type='hidden' name='uploadedPicture' value='" + data.fileName + "'>")
+
+            } else {
+                message.text("Что- то пошло не так");
+            }
+        },
+        complete: function () {
+            $('#picture').prop("disabled", false);
+            $('#submit').prop("disabled", false);
+        }
+    });
+
+};
