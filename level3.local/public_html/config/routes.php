51aa01e89a6bc39e17a2e6c22972e3fa4b59b2aa
NancySkrypnichenko <drachnancy@gmail.com>
2020-07-01 21:53:57 +0300

    search was added

--- level3.local/public_html/config/routes.php
+++ level3.local/public_html/config/routes.php
@@ -2,9 +2,10 @@
 return array(
     'book/([0-9]+)' => 'books/view/$1/$2',// action view in BooksController
     'books/([0-9]+)' => 'books/index/$1/$2', // action index in BooksController
+    'books/search' => 'books/search',
     'books' => 'books/index/0/0', // action index in BooksController
     'adm/form/([0-9]+)' => 'adm/form/$1/$2', // action Form in admСontroller
     'adm/form' => 'adm/form/0/0', // action Form in admСontroller
     'book/want/([0-9]+)' => 'books/statistic/$1/$2',
-    'adm/saveFile' => 'adm/saveFile'
+    'adm/saveFile' => 'adm/saveFile',
 );
